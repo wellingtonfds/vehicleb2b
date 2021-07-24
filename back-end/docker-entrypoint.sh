5cfe49010eaa42ecc8121881269ab13287c3521f
wellington fernandes <wellingtonfds@gmail.com>
2021-07-24 11:14:06 -0300

    up container back-end

---
+++ back-end/docker-entrypoint.sh
@@ -0,0 +1,18 @@
+
+echo "copy .env file:"
+file="./.env"
+if [ -f "$file" ]
+then
+	echo "$file found."
+	cp $file ./.env.example
+	echo ".env created"
+fi
+
+echo "Composer install"
+composer install
+
+echo "Run Migration"
+php artisan migrate
+
+echo "Run Server"
+php-fpm
