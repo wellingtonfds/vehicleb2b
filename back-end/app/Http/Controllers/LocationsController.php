42800dde214efde7968a6cdf087e08578a5a2857
arthurmr13@hotmail.com <A@Jr7ex3QKyUGNy2hpPRr&S&YJZ&u3WD>
2021-07-29 10:37:16 -0300

    Create stores CRUD with public profile too

--- back-end/app/Http/Controllers/LocationsController.php
+++ back-end/app/Http/Controllers/LocationsController.php
@@ -8,6 +8,7 @@ use App\Models\Location;
 use App\Models\User;
 use App\Services\Location\LocationService;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\Request;
 use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 
 class LocationsController extends Controller
@@ -20,6 +21,25 @@ class LocationsController extends Controller
     public function __construct(LocationService $locationService)
     {
         $this->service = $locationService;
+    }
+
+    public function update(Request $request, int $model)
+    {
+        // If it's not an admin
+        if (auth()->user()->type !== User::TYPE_ADMINISTRADOR) {
+            $isLocationOwner = Location::query()
+                ->where('id', $model)
+                ->whereHas('users', function (Builder $query) {
+                    $query->where('id', auth()->id());
+                })
+                ->exists();
+
+            if (!$isLocationOwner) {
+                throw new UnauthorizedHttpException('Você não tem permissão para ver esse local');
+            }
+        }
+
+        parent::update($request, $model);
     }
 
     public function show(int $model)
---
+++ back-end/app/Http/Controllers/StoresController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\StoreCollectionResource;
+use App\Http\Resources\StoreResource;
+use App\Models\Store;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
+
+class StoresController extends Controller
+{
+    public $resource = StoreResource::class;
+
+    public $resourceCollection = StoreCollectionResource::class;
+
+    public function update(Request $request, int $model)
+    {
+        // If it's not an admin
+        if (auth()->user()->type !== User::TYPE_ADMINISTRADOR) {
+            $isStoreOwner = Store::query()
+                ->where('id', $model)
+                ->whereHas('users', function (Builder $query) {
+                    $query->where('id', auth()->id());
+                })
+                ->exists();
+
+            if (!$isStoreOwner) {
+                throw new UnauthorizedHttpException('Você não tem permissão para ver essa loja');
+            }
+        }
+
+        parent::update($request, $model);
+    }
+
+    public function show(int $model)
+    {
+        // If it's not an admin
+        if (auth()->user()->type !== User::TYPE_ADMINISTRADOR) {
+            $isStoreOwner = Store::query()
+                ->where('id', $model)
+                ->whereHas('users', function (Builder $query) {
+                    $query->where('id', auth()->id());
+                })
+                ->exists();
+
+            if (!$isStoreOwner) {
+                throw new UnauthorizedHttpException('Você não tem permissão para ver essa loja');
+            }
+        }
+
+        parent::show($model);
+    }
+
+    public function getPublicProfile(Store $store)
+    {
+        return new $this->resource($store);
+    }
+}
